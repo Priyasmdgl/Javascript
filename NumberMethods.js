a10d96f51b3bd9435e5f8f2b013015317700c50d
Priyasmdgl <priyasmdgl99@gmail.com>
2025-02-19 11:53:07 +0530

    updated

--- NumberMethods.js
+++ NumberMethods.js
@@ -12,7 +12,7 @@ let num = 2.5697;
 console.log(num.toFixed(2)); 
 //to precision
 let num1 = 348.2569;
-console.log(num.toPrecision(3));
+console.log(num1.toPrecision(5));
 //is NaN
 console.log(isNaN("hello"));  
 console.log(isNaN(123));      
@@ -26,7 +26,7 @@ console.log(Math.ceil(4.1));
 //maximum value
 console.log(Math.max(1, 5, 3));  
 //miniumum value
-console.log(Math.max(1, 5, 3));  
+console.log(Math.min(1, 5, 3));  
 //returns power of the value
 console.log(Math.pow(2, 3));  
 //returns square root of the value
