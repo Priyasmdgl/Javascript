9448c64c32f6484a346f2744c7bdb5df38950e37
Priyasmdgl <priyasmdgl99@gmail.com>
2025-02-19 11:46:03 +0530

    updated

--- NumberMethods.js
+++ NumberMethods.js
@@ -7,6 +7,30 @@ let z=5e2; //exponential value
 console.log(x,y,z);
 //is integer
 console.log(Number.isInteger(a)); //checks the number is integer or not
+//to fixed
+let num = 2.5697;
+num.toFixed(2); 
+//to precision
+let num1 = 348.2569;
+num.toPrecision(3);
+//is NaN
+isNaN("hello");  
+isNaN(123);      
+//rounds to the nearest integer value
+Math.round(4.7);  
+Math.round(4.4);  
+//rounds down to the nearest integer value
+Math.floor(4.7);  
+//rounds up to the nearest integer value
+Math.ceil(4.1);  
+//maximum value
+Math.max(1, 5, 3);  
+//miniumum value
+Math.max(1, 5, 3);  
+//returns power of the value
+Math.pow(2, 3);  
+//returns square root of the value
+Math.sqrt(16);  
 //parseint
 let s="123";
 console.log(typeof(s));
